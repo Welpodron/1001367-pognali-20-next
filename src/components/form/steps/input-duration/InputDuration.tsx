548c1fd51ad4bc0a20598922ba9a975324f34111
welpodron <welpodron@gmail.com>
2023-02-07 16:48:31 +0300

    Обновление форм

--- src/components/form/steps/input-duration/InputDuration.tsx
+++ src/components/form/steps/input-duration/InputDuration.tsx
@@ -1,67 +1,42 @@
-import { useMemo, useState, useCallback, useEffect } from "react";
+import { useMemo, useState, useCallback } from "react";
 
 import {
   getCurrentDate,
   getDaysBetween,
-  getMaxDate,
   getMinDate,
-  isDateSame,
   setDay,
 } from "@/utils/time/time";
 
-import { Calendar } from "@/components/generic/calendar/Calendar";
+import { Calendar } from "@/components/generic/forms/Calendar/Calendar";
 import { InputNumber } from "../input-number/InputNumber";
 
 type InputDurationProps = {
   /** Контроль извне */
   state?: [
-    {
-      start: Date | null;
-      end: Date | null;
-    },
-    (value: { start: Date | null; end: Date | null }) => void
+    [Date | null, Date | null],
+    (value: [Date | null, Date | null]) => void
   ];
   /** Ошибка инпута */
   errors?: string;
 };
 
 export const InputDuration = ({ state, errors }: InputDurationProps) => {
-  const [outsideValue, setOutsideValue] = state ?? [];
-
   const currentCalendarDate = useMemo(() => getCurrentDate(), []);
 
-  const [calendarValue, setCalendarValue] = useState<
-    [Date | null, Date | null]
-  >([
-    outsideValue != null ? outsideValue.start : currentCalendarDate,
-    outsideValue != null
-      ? outsideValue.end
-      : setDay(currentCalendarDate, currentCalendarDate.getDate() + 2),
+  const [insideValue, setInsideValue] = useState<[Date | null, Date | null]>([
+    currentCalendarDate,
+    setDay(currentCalendarDate, currentCalendarDate.getDate() + 2),
   ]);
+  const [outsideValue, setOutsideValue] = state ?? [];
 
-  // outsideValue и setOutsideValue - меняются на каждом рендере поэтому их не нужно отслеживать в зависимостях
-  useEffect(() => {
-    if (setOutsideValue == null) {
-      return;
-    }
-
-    const filtered = calendarValue.filter((date) => date != null) as Date[];
-
-    if (filtered.length === 0) {
-      return setOutsideValue({ start: null, end: null });
-    }
-
-    const min = getMinDate(...filtered);
-    const max = getMaxDate(...filtered);
-
-    return isDateSame({ firstDate: min, secondDate: max })
-      ? setOutsideValue({ start: null, end: null })
-      : setOutsideValue({ start: min, end: max });
-  }, [calendarValue]);
+  const finalValue =
+    outsideValue != null && setOutsideValue != null
+      ? outsideValue
+      : insideValue;
 
   const handleInputChange = useCallback(
     (value: number) => {
-      const filtered = calendarValue.filter((date) => date != null) as Date[];
+      const filtered = finalValue.filter((date) => date != null) as Date[];
 
       if (filtered.length === 0) {
         return;
@@ -71,26 +46,28 @@ export const InputDuration = ({ state, errors }: InputDurationProps) => {
 
       // Поставить первую не нулевую минимальную дату
       if (value < 0 || isNaN(value)) {
-        return setCalendarValue([minDate, null]);
+        return setOutsideValue != null
+          ? setOutsideValue([minDate, null])
+          : setInsideValue([minDate, null]);
       }
 
       const newDate = setDay(minDate, minDate.getDate() + value);
 
-      setCalendarValue([minDate, newDate]);
+      setOutsideValue != null
+        ? setOutsideValue([minDate, newDate])
+        : setInsideValue([minDate, newDate]);
     },
-    [calendarValue, setCalendarValue]
+    [finalValue, setInsideValue, setOutsideValue]
   );
-
-  // debugger;
 
   return (
     <>
       <InputNumber
         state={[
-          calendarValue.every((date) => date != null)
+          finalValue.every((date) => date != null)
             ? getDaysBetween({
-                firstDate: calendarValue[0] as Date,
-                secondDate: calendarValue[1] as Date,
+                firstDate: finalValue[0] as Date,
+                secondDate: finalValue[1] as Date,
               })
             : NaN,
           handleInputChange,
@@ -100,9 +77,16 @@ export const InputDuration = ({ state, errors }: InputDurationProps) => {
         label="Длительность:"
         errors={errors}
       />
-      <Calendar state={[calendarValue, setCalendarValue]} />
+      <Calendar
+        state={[
+          finalValue,
+          setOutsideValue != null ? setOutsideValue : setInsideValue,
+        ]}
+      />
     </>
   );
 };
+
+InputDuration.whyDidYouRender = true;
 
 InputDuration.displayName = "Input.Duration";
